5a2282e3b772d8254d98d2edfd442b05d35de9a5
Kushal Das <61356005+Kushal997-das@users.noreply.github.com>
2020-09-21 18:34:50 +0530

    answer updated

---
+++ Jewel Game/answer.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+string s;
+cin>>s;
+int m=s.size(),i,n=0;
+for(i=0;i<m-1;i++)
+{
+if(s[i]==s[i+1])
+{
+n++;
+s.erase(s.begin()+i);
+s.erase(s.begin()+i);
+i=-1;
+m=m-2;
+
+}
+}
+cout<<n<<endl;
+
+
+}
+return 0;
+}
